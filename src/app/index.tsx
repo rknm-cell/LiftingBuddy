070c5ea994f443a04d31f97f39a48fc4149a15dd
rknm-cell <racknahm@gmail.com>
2024-06-27 22:06:33 -0400

    created foodcard

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hel</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{ 
+          backgroundColor: "gainsboro", 
+          padding: 10, 
+          borderRadius: 8,
+          gap: 5,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+
+      }}>
+        <View style={{flex: 1, backgroundColor: 'red'}}>
+          <Text style={{ fontWeight: "bold", fontSize: 20 }}>Poop</Text>
+          <Text style={{ color: "gray", fontSize: 16, paddingTop: 5 }}>
+            500 Calories, Domino's
+          </Text>
+        </View>
+        <MaterialIcons name="add-circle-outline" size={30} color="green" />
+      </View>
     </View>
   );
 }
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 10,
   },
 });
